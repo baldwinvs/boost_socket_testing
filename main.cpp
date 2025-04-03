2b5178a560fedfb5bc24b1b4446b05135b2a3906
Victor Baldwin <baldwinvs@outlook.com>
2025-04-03 00:09:39 -0700

    Add thread classes and others

--- main.cpp
+++ main.cpp
@@ -5,68 +5,199 @@
 #include <atomic>
 #include <cstdio>
 #include <cstddef>
-#include <cstring>
 #include <iomanip>
 #include <iostream>
+#include <string>
 
-constexpr uint16_t portToUse {141};
+constexpr uint16_t port {58585};
 constexpr size_t bufSize {16};
 inline const std::string address {"127.0.0.1"};
-std::atomic_bool received {true};
 
-int receiver() {
-    std::array<char, bufSize> buf {};
+struct SocketInfo
+{
+    SocketInfo(const std::string& ip, const uint16_t port, const bool receiver, const bool non_blocking)
+        : ip {ip}
+        , port {port}
+        , receiver {receiver}
+        , non_blocking {non_blocking}
+    {}
 
+    std::string ip {};
+    uint16_t port {};
+    bool receiver {};
+    bool non_blocking {};
+};
+
+class Wrapper_AsioUdp {
+private:
     boost::asio::io_service ioService;
     boost::asio::ip::udp::socket socket{ioService};
-    boost::asio::ip::udp::endpoint endpoint {boost::asio::ip::address::from_string(address), portToUse};
+    boost::asio::ip::udp::endpoint endpoint;
+    bool receiver;
+    bool non_blocking;
 
-    socket.open(boost::asio::ip::udp::v4());
-    socket.bind(endpoint);
-
-    while(1) {
-        auto len = socket.receive_from(boost::asio::buffer(buf.data(), sizeof(buf)), endpoint);
-        //print the data
-        std::cout.write(buf.data(), len);
-        std::cout << std::endl;
-        received = true;
-    }
-    return 0;
-}
-
-int transmitter() {
-    std::array<char, bufSize> buf {};
-    char fillChar {'0'};
-
-    boost::asio::io_service ioService;
-    boost::asio::ip::udp::socket socket{ioService};
-
-    socket.open(boost::asio::ip::udp::v4());
-
-    boost::asio::ip::udp::endpoint endpoint {boost::asio::ip::address::from_string(address), portToUse};
-
-    using namespace std::chrono_literals;
-    while(1) {
-        if(received) {
-            received = false;
-            memset(buf.data(), fillChar, buf.size());
-
-            socket.send_to(boost::asio::buffer(buf), endpoint);
-            if(++fillChar > '9')
-                fillChar = '0';
+public:
+    Wrapper_AsioUdp(const SocketInfo& socketInfo)
+        : ioService {}
+        , socket {ioService}
+        , endpoint {boost::asio::ip::address::from_string(socketInfo.ip), socketInfo.port}
+        , receiver {socketInfo.receiver}
+        , non_blocking {socketInfo.non_blocking}
+    {
+        socket.open(boost::asio::ip::udp::v4());
+        socket.non_blocking(non_blocking);
+        if (receiver) {
+            socket.bind(endpoint);
         }
-        std::this_thread::sleep_for(100ms);
     }
-    return 0;
-}
+
+    ~Wrapper_AsioUdp()
+    {
+        socket.close();
+    }
+
+    boost::asio::io_service& io_service()
+    {
+        return ioService;
+    }
+
+    size_t send(const char* const buf, const size_t size)
+    {
+        size_t bytesSent {};
+        if (!receiver) {
+            bytesSent = socket.send_to(boost::asio::buffer(buf, size), endpoint);
+        }
+        return bytesSent;
+    }
+
+    size_t recv(char* buf, const size_t size)
+    {
+        size_t bytesReceived {};
+        if (receiver) {
+            if (non_blocking) {
+                boost::system::error_code error;
+                bytesReceived = socket.receive_from(boost::asio::buffer(buf, size), endpoint, 0, error);
+
+                if (error == boost::asio::error::would_block) {
+                    //do nothing
+                } else if (error) {
+                    std::cerr << "Receive error: " << error.message() << std::endl;
+                } else {
+                    std::cerr << "Received " << bytesReceived << std::endl;
+                }
+            }
+            else {
+                bytesReceived = socket.receive_from(boost::asio::buffer(buf, size), endpoint);
+            }
+        }
+        return bytesReceived;
+    }
+};
+
+class ThreadBase {
+public:
+    ThreadBase() : running{false} {};
+    virtual ~ThreadBase()
+    {
+        stop();
+    }
+
+    inline void start()
+    {
+        running = true;
+        t = std::thread(&ThreadBase::run, this);
+    };
+    inline void stop() {
+        running = false;
+        if (t.joinable()) {
+            t.join();
+        }
+    }
+protected:
+    std::atomic_bool running;
+    virtual void run() = 0;
+private:
+    std::thread t;
+};
+
+template<size_t bufferSize>
+class TxThread : public ThreadBase
+{
+public:
+    TxThread(const SocketInfo& socketInfo, const std::chrono::milliseconds sleep_time)
+        : socketInfo {socketInfo}
+        , sleep_time {sleep_time}
+    {}
+
+    ~TxThread() override = default;
+
+private:
+    void run() override {
+        auto udp = Wrapper_AsioUdp(socketInfo);
+        while (running) {
+            auto bytes = udp.send(buf.data(), buf.size());
+            std::this_thread::sleep_for(sleep_time);
+        }
+    }
+
+    const SocketInfo socketInfo;
+    const std::chrono::milliseconds sleep_time;
+    std::array<char, bufferSize> buf;
+};
+
+template<size_t bufferSize>
+class RxThread : public ThreadBase
+{
+public:
+    explicit RxThread(const SocketInfo& socketInfo)
+        : socketInfo {socketInfo}
+    {}
+
+    ~RxThread() override = default;
+private:
+    void run() override
+    {
+        Wrapper_AsioUdp udp{socketInfo};
+        while (running) {
+            //because the socket is created within this function, cannot stop the socket, or it's io_service
+            //  doing it this way REQUIRE that the receiving socket is non-blocking.
+            auto bytes = udp.recv(buf.data(), buf.size());
+            //"the callback"
+            {
+                if (bytes > 0) {
+                    std::cout << "buffer = " << "0x";
+                    for (size_t i = 0; i < buf.size(); i++) {
+                        auto byte = static_cast<unsigned char>(buf.at(i));
+                        std::cout <<  std::hex << static_cast<int>(byte);
+                    }
+                    std::cout << std::endl;
+                }
+            }
+            //need a sleep if non-blocking
+            if (socketInfo.non_blocking) {
+                std::this_thread::sleep_for(std::chrono::microseconds(100));
+            }
+        }
+    }
+
+    const SocketInfo socketInfo;
+    std::array<char, bufferSize> buf;
+};
 
 int main() {
-    std::thread(receiver).detach();
-    std::thread(transmitter).detach();
+    const SocketInfo transmitInfo {address, port, false, false};
+    const SocketInfo receiveInfo {address, port, true, true};
 
-    while(1) {
-        using namespace std::chrono_literals;
-        std::this_thread::sleep_for(60s);
-    }
+    TxThread<bufSize> tx(transmitInfo, std::chrono::milliseconds{15});
+    RxThread<bufSize> rx(receiveInfo);
+
+    rx.start();
+    tx.start();
+
+    std::this_thread::sleep_for(std::chrono::seconds{30});
+
+    tx.stop();
+    rx.stop();
+
 	return 0;
 }
