aafa44bd576d48e4f5e7b771afc1612f2c8802d5
Victor Baldwin <baldwinvs@outlook.com>
2025-04-11 18:50:45 -0700

    Fix some formatting

--- main.cpp
+++ main.cpp
@@ -1,36 +1,38 @@
 #include <cstddef>
-#include <string>
-#include <iostream>
 #include <iomanip>
+#include <iostream>
+#include <string>
 
 #include "Thread/ReceiveSocketThread.h"
 #include "Thread/TransmitSocketThread.h"
 
-constexpr uint16_t port {58585};
-constexpr size_t bufSize {16};
-inline const std::string address {"127.0.0.1"};
+constexpr uint16_t port{58585};
+constexpr size_t bufSize{16};
+inline const std::string address{"127.0.0.1"};
 
 class CustomReceiverSocket : public ReceiveSocketThread
 {
-public:
-	CustomReceiverSocket(const SocketInfo& info, const SocketProperties properties)
-		: ReceiveSocketThread(info, properties)
-	{}
-	~CustomReceiverSocket() override = default;
+  public:
+    CustomReceiverSocket(const SocketInfo &info, const SocketProperties properties)
+        : ReceiveSocketThread(info, properties)
+    {
+    }
+    ~CustomReceiverSocket() override = default;
 
-private:
-	void receiveCallback(const size_t bytes) override
-	{
-		ReceiveSocketThread::receiveCallback(bytes);
-		std::cout << __PRETTY_FUNCTION__ << " received " << std::dec << bytes << " bytes" << std::endl;
-	}
+  private:
+    void receiveCallback(const size_t bytes) override
+    {
+        ReceiveSocketThread::receiveCallback(bytes);
+        std::cout << __PRETTY_FUNCTION__ << " received " << std::dec << bytes << " bytes" << std::endl;
+    }
 };
 
-int main() {
+int main()
+{
     auto transmitSocketProperties = determineSocketProperties(SocketType::tcp, false, true);
-    auto receiveSocketProperties  = determineSocketProperties(SocketType::tcp, true, true);
-    const SocketInfo transmitInfo {address, port, bufSize};
-    const SocketInfo receiveInfo {address, port, bufSize};
+    auto receiveSocketProperties = determineSocketProperties(SocketType::tcp, true, true);
+    const SocketInfo transmitInfo{address, port, bufSize};
+    const SocketInfo receiveInfo{address, port, bufSize};
 
     TransmitSocketThread tx(transmitInfo, transmitSocketProperties, std::chrono::milliseconds{1});
     CustomReceiverSocket rx(receiveInfo, receiveSocketProperties);
@@ -44,5 +46,5 @@ int main() {
     rx.stop();
     tx.stop();
 
-	return 0;
+    return 0;
 }
